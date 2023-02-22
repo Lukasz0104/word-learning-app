bc499fa6b0065778132a5d6db3f69195f28cd1e2
Lukasz0104 <lukasz.01.stepien@gmail.com>
2023-02-22 16:54:34 +0100

    Add ng-booststrap, create top navbar
    Change default paths for generating files with Angular CLI

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    declarations: [AppComponent, TopBarComponent],
+    imports: [BrowserModule, AppRoutingModule, NgbModule],
     providers: [],
     bootstrap: [AppComponent]
 })
