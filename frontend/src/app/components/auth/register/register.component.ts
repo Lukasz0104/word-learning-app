a0390122f765b1760788b89a320b92472a3ab1ba
Lukasz0104 <lukasz.01.stepien@gmail.com>
2023-03-23 22:42:34 +0100

    Add password missmatch validation

--- frontend/src/app/components/auth/register/register.component.ts
+++ frontend/src/app/components/auth/register/register.component.ts
@@ -1,29 +1,69 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import {
+    AbstractControl,
+    FormControl,
+    FormGroup,
+    ValidationErrors,
+    ValidatorFn,
+    Validators
+} from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
+
+const passwordMatchingValidator: ValidatorFn = (
+    control: AbstractControl
+): ValidationErrors | null => {
+    const password = control.get('password');
+    const repeatPassword = control.get('repeatPassword');
+
+    return password && repeatPassword && password.value === repeatPassword.value
+        ? null
+        : { passwordMismatch: true };
+};
 
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html'
 })
 export class RegisterComponent {
-    protected registerForm = this.fb.group({
-        username: new FormControl('', [Validators.required]),
-        password: new FormControl('', [
-            Validators.required,
-            Validators.minLength(8)
-        ]),
-        repeatPassword: new FormControl('', [
-            Validators.required,
-            Validators.minLength(8)
-        ]),
-        email: new FormControl('', [Validators.required, Validators.email])
-    });
-    // TODO add custom validator checking if repeat password is the same as password
+    protected registerForm = new FormGroup(
+        {
+            username: new FormControl('', [Validators.required]),
+            password: new FormControl('', [
+                Validators.required,
+                Validators.minLength(8)
+            ]),
+            repeatPassword: new FormControl('', [
+                Validators.required,
+                Validators.minLength(8)
+            ]),
+            email: new FormControl('', [Validators.required, Validators.email])
+        },
+        { validators: passwordMatchingValidator }
+    );
 
-    constructor(private fb: FormBuilder, private authService: AuthService) {}
+    constructor(private authService: AuthService) {}
 
     onRegister() {
         console.log('onRegister called');
+    }
+
+    protected get usernameControl() {
+        return this.registerForm.controls.username;
+    }
+
+    protected get passwordControl() {
+        return this.registerForm.controls.password;
+    }
+
+    protected get repeatPasswordControl() {
+        return this.registerForm.controls.repeatPassword;
+    }
+
+    protected get emailControl() {
+        return this.registerForm.controls.email;
+    }
+
+    protected get passwordMismatchError() {
+        return this.registerForm.errors?.['passwordMismatch'];
     }
 }
